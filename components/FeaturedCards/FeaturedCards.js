54d8b2c6a93daf5292c4b2004f31d90461b2afa5
Anthony Iritano <81189148+antiritano@users.noreply.github.com>
2022-11-16 17:39:03 -0500

    Card and featue card sections

---
+++ components/FeaturedCards/FeaturedCards.js
@@ -0,0 +1,9 @@
+const FeaturedCards = (props) => {
+  return (
+    <div>
+      <div className="flex flex-row items-center justify-center"></div>
+    </div>
+  );
+};
+
+export default FeaturedCards;
